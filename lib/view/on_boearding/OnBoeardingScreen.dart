e4ffe7348a2d6ce291190ad380754738e27248f3
mahmoud-altaramsi <mahmoudkhaledaltaramsi@gmail.com>
2025-02-24 18:45:40 +0200

    cereat app

---
+++ lib/view/on_boearding/OnBoeardingScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OnBoeardingScreen extends StatelessWidget {
+  const OnBoeardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
